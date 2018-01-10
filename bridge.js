43070561f200dd816e0b6492f64adf82063b16b4
sasalx <oyundanben@gmail.com>
2018-01-10 03:39:50 +0300

    Bridge Info

---
+++ bridge.js
@@ -0,0 +1,28 @@
+// Reqs.
+const huejay = require("huejay");
+const fs = require("fs");
+const dpPath = "./db.json"
+const db = require(dpPath);
+
+// Discover your bridge
+huejay.discover()
+  .then(bridges => {
+    for (let bridge of bridges) {
+      console.log(`Bridge Id: ${bridge.id}, Bridge IP: ${bridge.ip}`);
+      db.bridgeIP = bridge.ip;
+      jsonUpdate();
+    }
+  })
+  .catch(error => {
+    console.log(`Error: ${error.message}`);
+  });
+
+function jsonUpdate(){
+  fs.writeFile(dpPath, JSON.stringify(db, null, 2), function (err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log(JSON.stringify(db));
+    console.log('writing to ' + dpPath);
+  });
+}
